d0750768c08ec3c2231046a32a703745bd0ff0c0
Carlos Aguilar <carlosf.aguilarn@gmail.com>
2020-04-08 11:28:10 -0600

    quitar gitignore

--- laravel/resources/views/empleados/printPDF.blade.php
+++ laravel/resources/views/empleados/printPDF.blade.php
@@ -1,5 +1,6 @@
-<!-- Main content -->
-<link rel="stylesheet" href="{{ public_path('css/adminlte.min.css') }}">
+<!-- Main content --> 
+
+
 <div class="invoice p-3 mb-3">
 <!-- title row -->
 <div class="row">
@@ -16,35 +17,8 @@
 <!-- info row -->
 <div class="row invoice-info">
     <div class="col-sm-4 invoice-col">
-    From
-    <address>
-        <strong>Admin, Inc.</strong><br>
-        795 Folsom Ave, Suite 600<br>
-        San Francisco, CA 94107<br>
-        Phone: (804) 123-5432<br>
-        Email: info@almasaeedstudio.com
-    </address>
+   
     </div>
-    <!-- /.col -->
-    <div class="col-sm-4 invoice-col">
-    To
-    <address>
-        <strong>John Doe</strong><br>
-        795 Folsom Ave, Suite 600<br>
-        San Francisco, CA 94107<br>
-        Phone: (555) 539-1037<br>
-        Email: john.doe@example.com
-    </address>
-    </div>
-    <!-- /.col -->
-    <div class="col-sm-4 invoice-col">
-    <b>Invoice #007612</b><br>
-    <br>
-    <b>Order ID:</b> 4F3S8J<br>
-    <b>Payment Due:</b> 2/22/2014<br>
-    <b>Account:</b> 968-34567
-    </div>
-    <!-- /.col -->
 </div>
 <!-- /.row -->
 
@@ -104,29 +78,31 @@
     </div>
     <!-- /.col -->
     <div class="col-6">
-    <p class="lead">Amount Due 2/22/2014</p>
-
-    <div class="table-responsive">
-        <table class="table">
-        <tr>
-            <th style="width:50%">Subtotal:</th>
-            <td>$250.30</td>
-        </tr>
-        <tr>
-            <th>Tax (9.3%)</th>
-            <td>$10.34</td>
-        </tr>
-        <tr>
-            <th>Shipping:</th>
-            <td>$5.80</td>
-        </tr>
-        <tr>
-            <th>Total:</th>
-            <td>$265.24</td>
-        </tr>
-        </table>
-    </div>
+        <p class="lead">Amount Due 2/22/2014</p> 
+        <div class="table-responsive">
+            <table class="table">
+            <tr>
+                <th style="width:50%">Subtotal:</th>
+                <td>$250.30</td>
+            </tr>
+            <tr>
+                <th>Tax (9.3%)</th>
+                <td>$10.34</td>
+            </tr>
+            <tr>
+                <th>Shipping:</th>
+                <td>$5.80</td>
+            </tr>
+            <tr>
+                <th>Total:</th>
+                <td>$265.24</td>
+            </tr>
+            </table>
+        </div>
     </div>
     <!-- /.col -->
 </div>
 <!-- /.row --> 
+<button class="btn btn-primary">
+    Prueba
+</button>
